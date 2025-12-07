d3d8b2929eca468675a1338fa264d655301f8b69
Patmish <patmish.kumar@gmail.com>
2025-12-07 17:33:56 +0800

    changed altf4_main settings and core file as CORE

--- ALTF4_MAIN/CORE/wsgi.py
+++ ALTF4_MAIN/CORE/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for ALTF4_MAIN project.
+WSGI config for CORE project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ALTF4_MAIN.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CORE.settings')
 
 application = get_wsgi_application()
