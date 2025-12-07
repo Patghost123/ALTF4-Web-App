f5f1f629538d83553b9e2c414b1f6fee6d63c859
Muhyideen <muhammadmuhyideen@gmail.com>
2025-12-07 16:31:50 +0800

    base.html

---
+++ ALTF4_MAIN/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ALTF4_MAIN project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ALTF4_MAIN.settings')
+
+application = get_asgi_application()
