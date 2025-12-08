07ab825886ebf43653fd594db254a2bfbfad2c26
Sc4rZz77 <shaarving658@gmail.com>
2025-12-08 23:40:27 +0800

    reservations logic done

--- ALTF4_MAIN/reservations/urls.py
+++ ALTF4_MAIN/reservations/urls.py
@@ -4,9 +4,14 @@ from . import views
 app_name = 'reservations'
 
 urlpatterns = [
+    # New Admin Route
+    path('dashboard/', views.admin_dashboard, name='dashboard'),
+    
+    # New User Route
+    path('my-reservations/', views.my_reservations, name='my_reservations'),
+    
     path('reserve/<int:lab_id>/', views.make_reservation, name='make_reservation'),
     path('success/', views.reservation_success, name='success'),
     path('timetable/', views.timetable, name='timetable'),
-    # New API path for the calendar
     path('api/events/', views.all_reservations_api, name='all_reservations_api'),
 ]
